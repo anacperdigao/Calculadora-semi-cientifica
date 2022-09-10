a7756eb3592a7762099614b9262cf1cc011ecfcc
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-10 19:01:51 -0300

    Iniciando JS

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+//Puxando o input do cálculo
+const campoCalculo = document.querySelector(".conta-atual")
+
+//Puxando botões de números
+const botoesNumeros = document.querySelectorAll(".botao-num")
+
+//Evento de escuta dos botoes de numeros
+botoesNumeros.addEventListener('click', adicionaConta)
+
+
+function adicionaConta(){
+
+}
